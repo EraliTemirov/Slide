20ca5045c347c696f99753d0a8b15a6d3e42f170
Temirov Erali <eralitemirov129@gmail.com>
2024-06-28 12:40:25 +0500

    main

--- src/components/Detail/DetailMore.tsx
+++ src/components/Detail/DetailMore.tsx
@@ -1,15 +1,13 @@
+import React, {useEffect, useState} from 'react'
 import Reveal from 'reveal.js'
 import 'reveal.js/dist/reveal.css'
 import 'reveal.js/dist/theme/black.css'
-import {useEffect, useState} from 'react'
 import mainApiInstance from '../mainApiInstance'
 import {useParams} from 'react-router-dom'
 import IMG from '../../assets/react.svg'
 
 const DetailMore: React.FC = () => {
-  const {id} = useParams()
-  console.log('salom', id)
-
+  const {id} = useParams<{id: string}>()
   const [data, setData] = useState<any>(null)
   const [error, setError] = useState<Error | null>(null)
 
@@ -19,9 +17,9 @@ const DetailMore: React.FC = () => {
       const data = response.data.data
       setData(data)
       console.log(data)
-    } catch (error) {
-      setError(error as Error)
-      console.error(error)
+    } catch (err) {
+      setError(err as Error)
+      console.error(err)
     }
   }
 
@@ -31,7 +29,11 @@ const DetailMore: React.FC = () => {
     }
   }, [id])
 
-  console.log(data, 'supermen')
+  useEffect(() => {
+    if (data) {
+      Reveal.initialize()
+    }
+  }, [data])
 
   if (error) {
     return <div>Error fetching data: {error.message}</div>
@@ -42,40 +44,47 @@ const DetailMore: React.FC = () => {
   }
 
   return (
-    <div>
-      <div className='w-[1024px] m-auto'>
-        <div className='textr'>
-          <h2 className='text-center text-4xl mt-4'>
-            O'zbekiston Respublikasi Raqamli texnologiyalari vazirligi Muhammad al-Xorazmiy nomidagi
-            Toshkent axborot texnologiyalari universiteti
-          </h2>
-
-          <img src={IMG} alt='img' className='w-[30%] mx-auto mt-8' />
-          <p className='text-center text-3xl mt-10'>{data.name}</p>
-        </div>
-        <div className='text-right'>
-          <p>
-            <span>Gurux:</span> 124
-          </p>
-          <p>
-            <span>Bajardi:</span> Kimdir
-          </p>
-          <p>
-            <span>Tekshirdi:</span> Boshqasi
-          </p>
-        </div>
-        <p className='text-center text-2xl mt-10'>2024-yil</p>
-        {data.name && <p>{data.name}</p>}
-        {data.plans?.[1]?.name && <p>{data.plans[1].name}</p>}
+    <div className='reveal'>
+      <div className='slides'>
+        <section>
+          <div className='textr'>
+            <h2 className='text-center text-4xl mt-4'>
+              O'zbekiston Respublikasi Raqamli texnologiyalari vazirligi Muhammad al-Xorazmiy
+              nomidagi Toshkent axborot texnologiyalari universiteti
+            </h2>
+            <img src={IMG} alt='img' className='w-[30%] mx-auto mt-8' />
+            <p className='text-center text-3xl mt-10'>{data.name}</p>
+          </div>
+          <div className='text-right'>
+            <p>
+              <span>Gurux:</span> 124
+            </p>
+            <p>
+              <span>Bajardi:</span> Kimdir
+            </p>
+            <p>
+              <span>Tekshirdi:</span> Boshqasi
+            </p>
+          </div>
+          <p className='text-center text-2xl mt-10'>2024-yil</p>
+        </section>
+        {data.plans?.map((plan: any, index: number) => (
+          <section key={index}>
+            <h3>{plan.name}</h3>
+            {plan.description?.map((desc: any, descIndex: number) => (
+              <section key={descIndex}>
+                <h4>{desc.name}</h4>
+                {desc.content?.map((content: any, contentIndex: number) => (
+                  <section key={contentIndex}>
+                    <h5>{content.title}</h5>
+                    <p>{content.uzContent}</p>
+                  </section>
+                ))}
+              </section>
+            ))}
+          </section>
+        ))}
       </div>
-      {data.plans?.[1]?.description?.[0]?.name && <div>{data.plans[1].description[0].name}</div>}
-      {data.plans?.[1]?.description?.[0]?.content?.[1]?.title && (
-        <p>{data.plans[1].description[0].content[1].title}</p>
-      )}
-      <hr />
-      {data.plans?.[1]?.description?.[0]?.content?.[1]?.uzContent && (
-        <p>{data.plans[1].description[0].content[1].uzContent}</p>
-      )}
     </div>
   )
 }
