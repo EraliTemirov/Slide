f1952c269f16233bff49c422710269eb863559cf
Temirov Erali <eralitemirov129@gmail.com>
2024-07-02 22:51:04 +0500

    HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useCallback} from 'react'
 import Header from '../components/Header'
 import mainApiInstance from '../components/mainApiInstance'
 import {SearchBar} from '../components/SearchBar'
@@ -6,35 +6,33 @@ import {SearchResultsList} from '../components/SearchResultsList'
 import Footer from '../components/Footer'
 
 const HomePage: React.FC = () => {
-  const [results, setResults] = useState<any>([])
+  const [results, setResults] = useState<any[]>([])
   const [userData, setUserData] = useState<any>(null)
 
-  async function fetchData() {
+  const fetchData = useCallback(async () => {
     try {
       const res = await mainApiInstance.get('/auth/me')
       if (res.status === 200) {
-        console.log(res)
         setUserData(res.data)
         localStorage.setItem('token', res.data.token)
       }
     } catch (error) {
-      console.log(error)
+      console.error(error)
     }
-  }
+  }, [])
 
   useEffect(() => {
     fetchData()
-  }, [])
+  }, [fetchData])
 
   return (
     <>
       <Header userData={userData} />
-      <div className=' w-[90%] m-auto p-2 md:p-5 grid justify-items-center  shadow-[0_1px_0px_rgba(255, 255, 255, 0.1)]'>
+      <div className='w-[90%] m-auto p-2 md:p-5 grid justify-items-center shadow-[0_1px_0px_rgba(255,255,255,0.1)]'>
         <SearchBar setResults={setResults} />
         <SearchResultsList results={results} />
       </div>
-
-      <Footer results={undefined} />
+      <Footer />
     </>
   )
 }
